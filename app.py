d2452df22f285a5f78e61c4c5eccf3411ed9b851
salehaTanveer <56721766+salehaTanveer@users.noreply.github.com>
2020-08-27 09:16:11 +0500

    add routes

--- app.py
+++ app.py
@@ -7,8 +7,25 @@ app = Flask(__name__)
 
 #routes
 @app.route('/home')
-def welcome():
+def home():
     return render_template('Home.html')  
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html') 
+
+@app.route('/register')
+def register():
+    return render_template('register.html') 
+
+@app.route('/insert')
+def insert():
+    return render_template('insert.html') 
+
+@app.route('/search')
+def search():
+    return render_template('search.html') 
 
 
 #Run Server
